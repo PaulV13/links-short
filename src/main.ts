180d06531bf0bf13f84505195ab7b9cee2a434e2
Paul <paulvidart@gmail.com>
2024-02-11 19:24:56 -0300

    Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('API Acortador de URL').setDescription('API para acortar url para compartir en redes sociales.').setVersion('1.0').build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('', app, document)
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
