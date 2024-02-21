238af79e0627b20b59b68a15b7d1ec0844e701ba
Paul <paulvidart@gmail.com>
2024-02-21 20:20:39 -0300

    Delete configmodule for app and links module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { LinksModule } from './links/links.module'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    PrismaModule,
-    LinksModule,
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [PrismaModule, LinksModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
 import { LinksService } from './links.service'
 import { LinksController } from './links.controller'
-import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [LinksController],
   providers: [LinksService],
 })
